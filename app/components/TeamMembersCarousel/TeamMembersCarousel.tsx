cdf810dfca74c3b04f1b9935e266ad9dc69016d4
Jonathan Assi <assijonathan2@gmail.com>
2025-06-28 23:13:43 +0200

    Started carousel, need to show one card at a time, add arrow buttons and display all cards if the viewport is wide enough

--- app/components/TeamMembersCarousel/TeamMembersCarousel.tsx
+++ app/components/TeamMembersCarousel/TeamMembersCarousel.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import type { TeamMember } from './TeamMemberCard'
 import TeamMemberCard from './TeamMemberCard';
@@ -8,24 +9,76 @@ interface TeamMembersCarouselProps {
 }
 
 function TeamMembersCarousel({ teamMembers }: TeamMembersCarouselProps) {
+  const [scrollIndex, setScrollIndex] = React.useState(0);
+  const scrollRef = React.useRef<HTMLDivElement>(null);
+  const [cardsPerView, setCardsPerView] = React.useState(1);
+
+
+  const scrollToIndex = (index: number) => {
+    const scrollEl = scrollRef.current;
+    if (!scrollEl) return;
+
+    const itemWidth = scrollEl.firstElementChild?.clientWidth || 0;
+    scrollEl.scrollTo({
+      left: itemWidth * index,
+      behavior: 'smooth',
+    });
+    setScrollIndex(index);
+  };
+
+  React.useEffect(() => {
+    const scrollEl = scrollRef.current;
+    if (!scrollEl) return;
+
+    const onScroll = () => {
+      const scrollLeft = scrollEl.scrollLeft;
+      const itemWidth = scrollEl.firstElementChild?.clientWidth || 1;
+      const currentIndex = Math.round(scrollLeft / itemWidth);
+      setScrollIndex(currentIndex);
+    };
+
+    const updateCardsPerView = () => {
+    const width = window.innerWidth;
+    if (width >= 1024) {
+      setCardsPerView(3);
+    } else if (width >= 640) {
+      setCardsPerView(2);
+    } else {
+      setCardsPerView(1);
+    }
+  };
+
+    updateCardsPerView();
+    window.addEventListener('resize', updateCardsPerView);
+    scrollEl.addEventListener('scroll', onScroll);
+    return () => {
+      scrollEl.removeEventListener('scroll', onScroll);
+      window.removeEventListener('resize', updateCardsPerView);
+    }
+  }, []);
+
   return (
     <div className="mt-8">
-      <div className="flex items-center gap-5 overflow-x-auto pb-5 snap-x snap-mandatory scrollbar-hide">
+
+      <div ref={scrollRef} className="flex items-center gap-5 overflow-x-auto pb-5 snap-x snap-mandatory scrollbar-hide">
         {teamMembers.map((member, index) => (
           <TeamMemberCard key={index} member={member} />
         ))}
       </div>
+
       <div className="w-full flex justify-center mt-5">
         <div className="indicators flex items-center gap-2">
           {teamMembers.map((_, index) => (
             <button
               key={index}
-              className="indicator-item w-3 h-3 bg-primary rounded-full"
+              className={`indicator-item w-3 h-3 rounded-full transition-colors duration-300 ${scrollIndex === index ? 'bg-primary' : 'bg-custom-background-gray'} `}
               aria-label={`Go to member ${index + 1}`}
+              onClick={() => scrollToIndex(index)}
             />
           ))}
         </div>
       </div>
+
     </div>
   )
 }
